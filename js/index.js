7da27fe20bd2f40b1f775898347b30c6c072ccdc
jinimcoroneo <jinim1983@gmail.com>
2017-08-23 11:43:36 +0100

    add good and bad alert

--- js/index.js
+++ js/index.js
@@ -2,9 +2,18 @@ $( document ).ready(function () {
   $( "#headline-submit" ).click(function (event) {
     event.preventDefault();
     var headline = $( "#headline-input" )[0].value;
+    var headlineResponse = $("#headline-response")
 
     $.get("http://percepticon.herokuapp.com/scores/new", { title: headline }, function (response) {
-      // TODO: update visual feedback element in index.html with API response
+      headlineResponse.removeClass();
+      if(response.result === 1) {
+        headlineResponse.html ("bad")
+        headlineResponse.addClass("alert alert-danger")
+      } else if (response.result === 0) {
+        headlineResponse.html ("good")
+        headlineResponse.addClass("alert alert-success")
+      };
     });
   });
+
 });
